111cf67a1bea60a09356cf2d9066ca6f0c287fda
Arslan Saeed <arslan@hashicorp.com>
2019-01-15 04:08:30 -0500

    a

--- 3_create_VmResource/base.tf
+++ 3_create_VmResource/base.tf
@@ -5,6 +5,8 @@ resource "vsphere_folder" "folder" {
   type          = "vm"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
   tags = ["${vsphere_tag.tag.id}"]
+  custom_attributes = "${map(vsphere_custom_attribute.attribute.id, "${var.attributeValue}")}"
+
 }
 
 // Apply attribute to the folder
